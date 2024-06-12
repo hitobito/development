da638a5b000b1b8622a2352742cc3c73f3882037
Mountain Star <github@mtnstar.net>
2024-06-12 23:00:37 +0200

    Add test prep cmd

--- bin/dev-env.sh
+++ bin/dev-env.sh
@@ -7,27 +7,32 @@ source $SCRIPT_DIR/rails/_hit
 
 # Define the 'hit' command with subcommands
 function hit {
-    case "$1" in
-        test)
-            $SCRIPT_DIR/test/env
-            ;;
-        attach)
-            echo "Attaching to process..."
-            # Add your attach logic here
-            ;;
-        db)
-            shift
-            hit_db "$@"
-            ;;
-        rails)
-            shift
-            hit_rails "$@"
-            ;;
-        *)
-            echo "Usage: hit {test|attach|db}"
-            return 1
-            ;;
-    esac
+  case "$1" in
+    test)
+      if [[ "$2" == "prep" ]]; then
+        $SCRIPT_DIR/test/prep
+      else
+        $SCRIPT_DIR/test/env
+      fi
+
+      ;;
+    attach)
+      echo "Attaching to process..."
+      # Add your attach logic here
+      ;;
+    db)
+      shift
+      hit_db "$@"
+      ;;
+    rails)
+      shift
+      hit_rails "$@"
+      ;;
+    *)
+      echo "Usage: hit {test|attach|db}"
+      return 1
+      ;;
+  esac
 }
 
 # Export the command so it can be used in the current session
