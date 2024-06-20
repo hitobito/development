2f4bb97b2f077b908e27bbe09295f6a1a17c92e4
Mountain Star <github@mtnstar.net>
2024-06-20 14:45:04 +0200

    Add welcome banner

--- bin/dev-env.sh
+++ bin/dev-env.sh
@@ -2,14 +2,19 @@
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]:-$_}")")"
 HIT_SCRIPT_DIR=$SCRIPT_DIR/hit
 HIT_PROJECT="$(basename $(cd $SCRIPT_DIR/../ && pwd))"
+HIT_WAGON_NAMES=$(ls -d "$HIT_SCRIPT_DIR"/../../app/hitobito_* | sed 's|.*/hitobito_||')
 export HIT_SCRIPT_DIR
 export HIT_PROJECT
+export HIT_WAGON_NAMES
 source $SCRIPT_DIR/hit/test/_hit
 source $SCRIPT_DIR/hit/db/_hit
 source $SCRIPT_DIR/hit/rails/_hit
 source $SCRIPT_DIR/hit/dev/_hit
+source $SCRIPT_DIR/hit/welcome_info
 
 PS1="HIT-$HIT_PROJECT> "
+
+hit_welcome_info
 
 hit() {
   cd $SCRIPT_DIR/../
