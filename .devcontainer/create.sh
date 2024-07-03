8a219e3c2813e2d1ca3f8b96d6b0a7b9777001c9
Robin Bühler <git+r@obin.ch>
2024-07-03 14:56:11 +0200

    add ruby-lsp gem

--- .devcontainer/create.sh
+++ .devcontainer/create.sh
@@ -3,5 +3,6 @@
 # This script will be run _inside_ the container everytime you create a devontainer. This happens after the initialize.sh script.
 
 sudo gem install debug
+sudo gem install ruby-lsp
 
 /usr/local/bin/rails-entrypoint.sh echo "Finished initializing!"
