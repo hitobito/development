dfb65848715ba61c8b46a6d2baa02cbe9d864040
Chris <76159444+hunchr@users.noreply.github.com>
2024-09-05 05:33:05 +0000

    Always use hitobito as hit project

--- bin/dev-env.sh
+++ bin/dev-env.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]:-$_}")")"
+SCRIPT_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]:-$_}")")
 
-bash --rcfile $SCRIPT_DIR/hit/_hit
+bash --rcfile "$SCRIPT_DIR/hit/_hit"
