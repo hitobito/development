e67d3c46b13225d02114a7cbb3f93a19a89e7cda
Robin <git+r@obin.ch>
2024-09-30 14:27:11 +0200

    add rubocop

--- .devcontainer/create.sh
+++ .devcontainer/create.sh
@@ -2,8 +2,8 @@
 
 # This script will be run _inside_ the container everytime you create a devontainer. This happens after the initialize.sh script.
 
-gem install debug
-gem install ruby-lsp
-gem install rubocop rubocop-rails rubocop-rspec
+sudo gem install debug
+sudo gem install ruby-lsp
+sudo gem install rubocop rubocop-rails rubocop-rspec
 
 /usr/local/bin/rails-entrypoint.sh
